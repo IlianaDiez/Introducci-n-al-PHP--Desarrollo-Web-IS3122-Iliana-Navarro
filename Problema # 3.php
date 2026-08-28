3dc98649681329e4dbb615a2e20d1eeee5c763d6
IlianaDiez <iliananavarro206@gmail.com>
2026-08-28 08:22:11 -0500

    Add files via upload

---
+++ Problema # 3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Información de PHP</title>
+</head>
+<body>
+
+    <?php
+    // Problema 3: Mostrar la configuración completa de PHP en el servidor local
+    phpinfo();
+    ?>
+
+</body>
+</html>
