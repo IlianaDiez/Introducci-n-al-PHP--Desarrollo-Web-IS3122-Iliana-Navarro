2161443a9b9669d41a4d61ea5fbb8f2aa543227b
IlianaDiez <iliananavarro206@gmail.com>
2026-09-04 08:50:33 -0500

    Enhance HTML output with styling and strong tags
    Updated HTML structure and added inline styles for better presentation.

--- Variables.php
+++ Variables.php
@@ -1,6 +1,6 @@
 <?php
 $author1 = "John Doe";
 $author2 = "Max Mustermann";
-echo "<h1>Hello World!</h1> 
-<p>This dynamic web page was created by $author1 and $author2.</p>";
+echo "<h1 style='color: #2c3e50;'>Hello World!</h1> 
+<p style='color: #16a085;'>This dynamic web page was created by <strong style='color: #e74c3c;'>$author1</strong> and <strong style='color: #e74c3c;'>$author2</strong>.</p>";
 ?>
